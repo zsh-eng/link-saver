527de60d12d07db7f5787ae3c917a6eb78b5899a
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-01 08:05:12 +0800

    feat: add telegraph api createPage

---
+++ telegraph/api.ts
@@ -0,0 +1,42 @@
+import { load } from 'https://deno.land/std@0.221.0/dotenv/mod.ts';
+import { DOMParser } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
+import { bodyToNodes } from '/telegraph/md-to-node.ts';
+
+const BASE_URL = 'https://api.telegra.ph';
+const env = await load();
+
+const doc = new DOMParser().parseFromString(
+  `
+  <h1>Hello World!</h1>
+  <p>Hello from <a href="https://deno.land/">Deno!</a></p>
+`,
+  'text/html'
+)!;
+
+const token = env['TELEGRAPH_TOKEN'];
+
+async function createPage(markdownString: string) {
+  if (!token) throw new Error('TELEGRAPH_TOKEN is required');
+
+  const content = bodyToNodes(doc.body)!;
+  const url = new URL('createPage', BASE_URL);
+
+  console.log(JSON.stringify(content));
+
+  const params = new URLSearchParams();
+  params.set('access_token', token);
+  params.set('title', 'Hello World!');
+  params.set('content', JSON.stringify(content));
+  url.search = params.toString();
+
+  const res = await fetch(url.href, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await res.json();
+  console.log(data);
+}
+
+createPage('');
---
+++ telegraph/md-to-node.ts
@@ -0,0 +1,76 @@
+import {
+  Node,
+  CharacterData,
+  Element,
+} from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
+
+/**
+ * TelegraphNode represents a node in the format used by the Telegraph API.
+ * @see {@link https://telegra.ph/api#Content-format | Content Format}
+ */
+type TelegraphNode =
+  | {
+      tag: string;
+      attrs?: Record<string, string>;
+      children?: TelegraphNode[];
+    }
+  | string
+  | false;
+
+function isTextNode(node: Node): node is CharacterData {
+  return node.nodeType == node.TEXT_NODE;
+}
+
+function isElementNode(node: Node): node is Element {
+  return node.nodeType == node.ELEMENT_NODE;
+}
+
+function domToNode(domNode: Node) {
+  if (isTextNode(domNode)) {
+    return domNode.data;
+  }
+
+  if (!isElementNode(domNode)) {
+    return false;
+  }
+
+  const nodeElement: TelegraphNode = {
+    tag: domNode.tagName.toLowerCase(),
+  };
+
+  for (let i = 0; i < domNode.attributes.length; i++) {
+    const attr = domNode.attributes[i];
+    if (attr.name == 'href' || attr.name == 'src') {
+      if (!nodeElement.attrs) {
+        nodeElement.attrs = {};
+      }
+      nodeElement.attrs[attr.name] = attr.value;
+    }
+  }
+
+  if (domNode.childNodes.length > 0) {
+    nodeElement.children = [];
+    for (let i = 0; i < domNode.childNodes.length; i++) {
+      const childDomNode = domNode.childNodes[i];
+      const childTelegraphNode = domToNode(childDomNode);
+      nodeElement.children.push(childTelegraphNode);
+    }
+  }
+
+  return nodeElement;
+}
+
+export function bodyToNodes(body: Node): TelegraphNode[] {
+  const empty = [''];
+  const node = domToNode(body);
+
+  if (typeof node === 'string') {
+    return [node];
+  }
+
+  if (node === false) {
+    return empty; // Must provide content - [false] is not allowed
+  }
+
+  return node.children ?? empty;
+}
