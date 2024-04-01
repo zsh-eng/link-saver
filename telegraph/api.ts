ddec7c69c1e8eb59170790f431c171c3fd9d805b
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-02 07:58:07 +0800

    feat: add micromark for md to html

--- telegraph/api.ts
+++ telegraph/api.ts
@@ -1,23 +1,40 @@
 import { load } from 'https://deno.land/std@0.221.0/dotenv/mod.ts';
 import { DOMParser } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
 import { bodyToNodes } from '/telegraph/md-to-node.ts';
+import { micromark } from 'https://esm.sh/micromark';
 
 const BASE_URL = 'https://api.telegra.ph';
 const env = await load();
 
-const doc = new DOMParser().parseFromString(
-  `
-  <h1>Hello World!</h1>
-  <p>Hello from <a href="https://deno.land/">Deno!</a></p>
-`,
-  'text/html'
-)!;
+const teststring = `
+## Deno
+
+[[Deno]]
+
+
+## Supabase
+
+I have to set up the Supabase tables and project.
+Details are not provided in the documentation.
+
+
+## Grammy
+
+### Sessions
+
+[Sessions and Storing Data (built-in) | grammY](https://grammy.dev/plugins/session.html)
+Sessions are necessary because Telegram bots don't manage sessions - i.e. doesn't have access to old messages.
+
+`;
 
 const token = env['TELEGRAPH_TOKEN'];
-
 async function createPage(markdownString: string) {
   if (!token) throw new Error('TELEGRAPH_TOKEN is required');
 
+  const doc = new DOMParser().parseFromString(
+    micromark(markdownString.replace(/\n/g, '\r\n')),
+    'text/html'
+  )!;
   const content = bodyToNodes(doc.body)!;
   const url = new URL('createPage', BASE_URL);
 
@@ -39,4 +56,4 @@ async function createPage(markdownString: string) {
   console.log(data);
 }
 
-createPage('');
+createPage(teststring);
