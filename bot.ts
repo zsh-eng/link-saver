d5315cc5e8e325ff2840bd07361503c56cb93a8a
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-02 23:19:25 +0800

    feat: set up initial bot

--- bot.ts
+++ bot.ts
@@ -1,19 +1,40 @@
+import { createPageFromHtml } from '/telegraph/api.ts';
+import { OmnivoreClient } from '/omnivore/api.ts';
 import { Bot } from 'https://deno.land/x/grammy@v1.21.2/mod.ts';
 import { load } from 'https://deno.land/std@0.221.0/dotenv/mod.ts';
 
 const env = await load();
 const bot = new Bot(env['BOT_TOKEN']);
 
+const client = new OmnivoreClient(env['OMNIVORE_TOKEN'] ?? '');
+
 // You can now register listeners on your bot object `bot`.
 // grammY will call the listeners when users send messages to your bot.
 
 // Handle the /start command.
 bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
+
+// Handle URL messages
+bot.on('message:entities:url', async (ctx) => {
+  const urlEntity = ctx.message.entities.find((e) => e.type === 'url')!;
+  const url = ctx.message.text.slice(
+    urlEntity.offset,
+    urlEntity.offset + urlEntity.length
+  );
+
+  const html = await client.saveUrlAndGetArticleHtml(url);
+  if (!html) {
+    ctx.reply('Failed to save the URL.');
+    return;
+  }
+  const telegraphUrl = await createPageFromHtml(html);
+  ctx.reply(`You sent me a URL: ${telegraphUrl}`);
+});
+
 // Handle other messages.
-bot.on('message', (ctx) => ctx.reply('Got another message!'));
+// bot.on('message', (ctx) => ctx.reply('Got another message!'));
 
 // Now that you specified how to handle messages, you can start your bot.
 // This will connect to the Telegram servers and wait for messages.
-
 // Start the bot.
 bot.start();
